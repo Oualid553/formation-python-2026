ad22268dfe590f9978d0c52d1950a25977570ca9
Oualid553 <oualidkassi@gmail.com>
2026-02-15 14:21:39 +0100

    feat(section-12): complete exercise 1 - create native objects

---
+++ 01-fondations/section-12-types-natifs/exercices/exercice_01_creer_objets.py
@@ -0,0 +1,29 @@
+# Exercice 1 : Créer des objets natifs
+# Section 12 - Formation Docstring
+# Oualid - 15 février 2026
+
+# Créer un objet de type chaîne de caractères
+chaine = 
+# Créer un objet de type nombre entier
+nombre_entier = 
+# Créer objet de type nombre décimal
+nombre_decimal = 
+# Créer un objet de type booléen
+boolean =
+
+# Mon code
+chaine = "Hello"
+nombre_entier = 60
+nombre_decimal = 60.5
+boolean = True
+
+# Exemple réel :
+nom = "Oualid"
+age = 36
+prix = 45.50
+est_actif = True
+
+print(nom)
+print(age)
+print(prix)
+print(est_actif)
