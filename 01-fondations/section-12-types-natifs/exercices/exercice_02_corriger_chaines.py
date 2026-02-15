593dc6bd56feac81d08b9ac2173912956a36e4cd
Oualid553 <oualidkassi@gmail.com>
2026-02-15 14:31:37 +0100

    feat(section-12): complete exercise 2 - fix string errors

---
+++ 01-fondations/section-12-types-natifs/exercices/exercice_02_corriger_chaines.py
@@ -0,0 +1,17 @@
+# Exercice 2 : Corriger les erreurs des chaînes de caractères
+# Section 12 - Formation Docstring
+# Oualid - 15 février 2026
+
+# CODE DE DÉPART (AVEC ERREURS) :
+print('Bonjour tout le monde !")
+print('Je m'appelle Patrick.')
+print("Je dis souvent "la vie est un long fleuve tranquille" quand on s'appelle Patrick.")
+print(Oui, je suis très philosophe.)
+
+#Code corrigé 
+print("Bonjour tout le monde !")
+print('Je m\'appelle Patrick.')
+print("Je dis souvent 'la vie est un long fleuve tranquille' quand on s'appelle Patrick.")
+print("Oui, je suis très philosophe.")
+
+#Il fallait bien faire attention aux guillemets.
